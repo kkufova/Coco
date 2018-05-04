b9abacd72b47aa00edb048fe5049116a9f0d4a58
Klára Kufová <kkufova@redhat.com>
2018-05-04 16:00:16 +0200

    Implement rule pruning.

--- src/main/java/dialog/Dialog.java
+++ src/main/java/dialog/Dialog.java
@@ -2,6 +2,8 @@ package dialog;
 
 import java.util.logging.Logger;
 
+import dialog.adaptation.RulePruner;
+
 /**
  * The main class of the Coco dialog system.
  * Runs each part of the dialog.
@@ -19,26 +21,34 @@ public class Dialog {
 
     public static void main(String[] args) throws Exception {
         DialogFactory factory = new DialogFactory();
+        RulePruner rulePruner = new RulePruner();
+
+        String pathToGrammars = "src/main/resources/speech-recognition/grammars/";
 
         DialogPart openingDialog = factory.getDialogPart(dialog.enumerations.DialogPart.OPENING);
         dialogPart = dialog.enumerations.DialogPart.OPENING.toString();
         openingDialog.actDialogPart();
+        rulePruner.pruneGrammar(pathToGrammars + openingDialog.getDialogPartGrammar() + ".gram");
 
         DialogPart countingDialog = factory.getDialogPart(dialog.enumerations.DialogPart.COUNTING);
         dialogPart = dialog.enumerations.DialogPart.COUNTING.toString();
         countingDialog.actDialogPart();
+        rulePruner.pruneGrammar(pathToGrammars + countingDialog.getDialogPartGrammar() + ".gram");
 
         DialogPart orderingDialog = factory.getDialogPart(dialog.enumerations.DialogPart.ORDERING);
         dialogPart = dialog.enumerations.DialogPart.ORDERING.toString();
         orderingDialog.actDialogPart();
+        rulePruner.pruneGrammar(pathToGrammars + orderingDialog.getDialogPartGrammar() + ".gram");
 
         DialogPart shippingDialog = factory.getDialogPart(dialog.enumerations.DialogPart.SHIPPING);
         dialogPart = dialog.enumerations.DialogPart.SHIPPING.toString();
         shippingDialog.actDialogPart();
+        rulePruner.pruneGrammar(pathToGrammars + shippingDialog.getDialogPartGrammar() + ".gram");
 
         DialogPart closingDialog = factory.getDialogPart(dialog.enumerations.DialogPart.CLOSING);
         dialogPart = dialog.enumerations.DialogPart.CLOSING.toString();
         closingDialog.actDialogPart();
+        rulePruner.pruneGrammar(pathToGrammars + closingDialog.getDialogPartGrammar() + ".gram");
     }
 
 }
--- src/main/java/dialog/adaptation/RulePruner.java
+++ src/main/java/dialog/adaptation/RulePruner.java
@@ -1,5 +1,58 @@
 package dialog.adaptation;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class RulePruner {
+
+    public void pruneGrammar(String grammarFile) {
+        // Based on the application requirements, use one or both of the following options:
+        pruneOldRules(grammarFile);
+        pruneUnusedRules(grammarFile);
+    }
+
+    private void pruneOldRules(String grammarFile) {
+        try {
+            File file = new File(grammarFile);
+            FileReader fileReader = new FileReader(grammarFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+
+            Pattern pattern = Pattern.compile("public <generated-rule-" + "(\\d{4}-\\d{2}-\\d{2}-\\d{2}-\\d{2}-\\d{2})" + "> = ");
+
+            while ((line = bufferedReader.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    String ruleTimestamp = matcher.group(1);
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss");
+                    LocalDateTime ruleDateAndTime = LocalDateTime.parse(ruleTimestamp, formatter);
+
+                    if (ruleDateAndTime.isBefore(LocalDateTime.now().minusMonths(1))) { // Set the number of month to suite your application's needs.
+                        List<String> out = Files.lines(file.toPath())
+                                .filter(rule -> !rule.contains(ruleTimestamp))
+                                .collect(Collectors.toList());
+                        Files.write(file.toPath(), out, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
+                    }
+                }
+            }
+
+        } catch (IOException exception) {
+            System.err.println("An error occurred during the deletion of the obsolete rule.");
+        }
+    }
+
+    private void pruneUnusedRules(String grammarFile) {
+        // TODO: implement rule pruning based on the usage of each rule.
+    }
 
 }
