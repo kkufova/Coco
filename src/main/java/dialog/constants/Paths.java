f58b2f0ccf5239182b6e8a430849bcec287d1ea0
Klára Kufová <kkufova@redhat.com>
2018-05-10 16:51:07 +0200

    Prepare Coco for distribution.

--- src/main/java/dialog/constants/Paths.java
+++ src/main/java/dialog/constants/Paths.java
@@ -15,4 +15,13 @@ public final class Paths {
     public static final String CATEGORY_GRAMMARS = CURRENT_DIRECTORY + COCO_DIRECTORY + "speech-recognition/grammars/category-grammars/";
     public static final String LANGUAGE_MODEL = CURRENT_DIRECTORY + COCO_DIRECTORY + "speech-recognition/language-model/en-70k-0.2-pruned.lm";
 
+    // If you want to work with the application in your IDE, use these paths instead:
+
+    //public static final String LOGGING = "src/main/resources/logging/logging.properties";
+    //public static final String ACOUSTIC_MODEL = "src/main/resources/speech-recognition/acoustic-model";
+    //public static final String DICTIONARY = "src/main/resources/speech-recognition/dictionary/cmudict-en-us.dict";
+    //public static final String GRAMMARS = "src/main/resources/speech-recognition/grammars/";
+    //public static final String CATEGORY_GRAMMARS = "src/main/resources/speech-recognition/grammars/category-grammars/";
+    //public static final String LANGUAGE_MODEL = "src/main/resources/speech-recognition/language-model/en-70k-0.2-pruned.lm";
+
 }
